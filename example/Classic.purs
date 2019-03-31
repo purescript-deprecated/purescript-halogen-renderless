42535e9e77c6d4cc812fdc2aebf65c385e69f83d
Thomas Honeyman <admin@thomashoneyman.com>
2019-03-31 14:24:55 -0700

    it's a brave new extensible action world

--- example/Classic.purs
+++ example/Classic.purs
@@ -7,15 +7,17 @@ import Data.Const (Const)
 import Data.Maybe (Maybe(..))
 import Data.Set as Set
 import Data.Symbol (SProxy(..))
+import Data.Variant (Variant, inj, match)
 import Effect.Class (class MonadEffect)
 import Example.Renderless.TagInput as TagInput
 import Halogen (defaultEval)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Renderless.State (modifyState_)
 
 type ChildSlots =
-  ( inner :: TagInput.Slot (Const Void) () Unit )
+  ( tagInput :: TagInput.Slot (Const Void) () Unit )
 
 component :: ∀ m. MonadEffect m => H.Component HH.HTML (Const Void) Unit Void m
 component =
@@ -28,17 +30,35 @@ component =
   render :: Unit -> H.ComponentHTML Void ChildSlots m
   render _ = 
     HH.div_ 
-      [ HH.slot (SProxy :: SProxy "inner") unit TagInput.component { render: render' } (\_ -> Nothing)
-      ]
+      [ HH.slot (SProxy :: _ "tagInput") unit (TagInput.component handleExtraActions) { render: renderTagInput } (\_ -> Nothing) ]
 
-  render' 
+  renderTagInput
     :: TagInput.State 
-    -> H.ComponentHTML (TagInput.Action (Const Void) () m) () m
-  render' st@{ tags } = 
+    -> H.ComponentHTML (TagInput.Action (Const Void) () m ExtraActions) () m
+  renderTagInput st@{ tags } = 
     HH.div_ 
       [ HH.input $ TagInput.attachInputProps st [] 
       , HH.div_ $ Set.toUnfoldable tags # map \x -> 
           HH.li 
-            [ HE.onClick \_ -> Just (TagInput.RemoveTag x) ] 
+            [ HE.onClick \_ -> Just (removeTag x) ] 
             [ HH.text x ]
       ]
+
+  removeTag :: String -> TagInput.Action (Const Void) () m ExtraActions
+  removeTag = TagInput.Action <<< inj (SProxy :: _ "removeTag") 
+ 
+
+type HandleExtraActions pq ps m v = 
+  H.HalogenM (TagInput.StateStore pq ps m v) (TagInput.Action pq ps m v) ps (TagInput.Message pq) m Unit
+
+type ExtraActions = 
+  ( removeTag :: String )
+
+handleExtraActions 
+  :: forall m
+   . Variant ExtraActions 
+  -> HandleExtraActions (Const Void) () m ExtraActions
+handleExtraActions = match
+  { removeTag: \tag ->
+      modifyState_ \st -> st { tags = Set.delete tag st.tags }
+  }
--- example/Renderless/TagInput.purs
+++ example/Renderless/TagInput.purs
@@ -7,8 +7,10 @@ import Control.Comonad.Store (Store, store)
 import Control.Monad.Free (liftF)
 import DOM.HTML.Indexed (HTMLinput)
 import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, unwrap)
 import Data.Set (Set)
 import Data.Set as Set
+import Data.Variant (SProxy(..), Variant, inj, onMatch)
 import Effect.Class (class MonadEffect)
 import Halogen (defaultEval)
 import Halogen as H
@@ -20,12 +22,27 @@ import Renderless.State (getState, modifyState_, modifyStore_)
 import Web.Event.Event (preventDefault)
 import Web.UIEvent.KeyboardEvent as KE
 
-data Action pq ps m
-  = HandleInput String
-  | HandleKey KE.KeyboardEvent
-  | RemoveTag String
-  | Receive (Input pq ps m)
-  | Raise (pq Unit)
+newtype Action pq ps m v = Action (Variant
+  ( handleInput :: String 
+  , handleKey :: KE.KeyboardEvent
+  , receive :: Input pq ps m v
+  , raise :: pq Unit
+  | v
+  ))
+
+derive instance newtypeAction :: Newtype (Action pq ps m v) _
+
+handleInput :: forall pq ps m v. String -> Action pq ps m v
+handleInput = Action <<< inj (SProxy :: _ "handleInput")
+
+handleKey :: forall pq ps m v. KE.KeyboardEvent -> Action pq ps m v
+handleKey = Action <<< inj (SProxy :: _ "handleKey")
+
+receive :: forall pq ps m v. Input pq ps m v -> Action pq ps m v
+receive = Action <<< inj (SProxy :: _ "receive")
+  
+raise :: forall pq ps m v. pq Unit -> Action pq ps m v
+raise = Action <<< inj (SProxy :: _ "raise")
 
 data Query ps a
   = Send (ChildQueryBox ps (Maybe a)) 
@@ -33,79 +50,79 @@ data Query ps a
 type Slot pq ps
   = H.Slot (Query ps) (Message pq)
 
-type StateStore pq ps m =
-  Store State (H.ComponentHTML (Action pq ps m) ps m)
+slotLabel :: SProxy "tagInput"
+slotLabel = SProxy
+
+type StateStore pq ps m v =
+  Store State (H.ComponentHTML (Action pq ps m v) ps m)
 
 type State = 
   { tags :: Set String 
   , text :: String 
   }
 
-type Input pq ps m =
-  { render :: State -> H.ComponentHTML (Action pq ps m) ps m 
+type Input pq ps m v =
+  { render :: State -> H.ComponentHTML (Action pq ps m v) ps m 
   }
 
 data Message pq
   = Emit (pq Unit)
 
 attachInputProps 
-  :: forall pq ps m
+  :: forall pq ps m v
    . State 
-  -> Array (HH.IProp HTMLinput (Action pq ps m)) 
-  -> Array (HH.IProp HTMLinput (Action pq ps m))
+  -> Array (HH.IProp HTMLinput (Action pq ps m v)) 
+  -> Array (HH.IProp HTMLinput (Action pq ps m v))
 attachInputProps st = append 
   [ HP.value st.text
-  , HE.onValueInput $ pure <<< HandleInput
-  , HE.onKeyDown $ pure <<< HandleKey
+  , HE.onValueInput $ pure <<< handleInput
+  , HE.onKeyDown $ pure <<< handleKey
   ]
 
 component 
-  :: ∀ pq ps m
+  :: ∀ pq ps m v
    . MonadEffect m
-  => H.Component HH.HTML (Query ps) (Input pq ps m) (Message pq) m
-component =
+  => (Variant v -> H.HalogenM (StateStore pq ps m v) (Action pq ps m v) ps (Message pq) m Unit)
+  -> H.Component HH.HTML (Query ps) (Input pq ps m v) (Message pq) m
+component handleExtraAction =
   H.mkComponent
     { initialState
     , render: extract
     , eval: H.mkEval $ defaultEval
         { handleAction = handleAction
         , handleQuery = handleQuery
-        , receive = Just <<< Receive
+        , receive = Just <<< receive
         }
     }
   where
-  initialState :: Input pq ps m -> StateStore pq ps m
+  initialState :: Input pq ps m v -> StateStore pq ps m v
   initialState { render } = store render { tags: Set.fromFoldable [ "my-example-tag" ], text: "" }
 
   handleAction
-    :: Action pq ps m
-    -> H.HalogenM (StateStore pq ps m) (Action pq ps m) ps (Message pq) m Unit
-  handleAction = case _ of
-    HandleInput str ->
-      modifyState_ _ { text = str }
+    :: Action pq ps m v
+    -> H.HalogenM (StateStore pq ps m v) (Action pq ps m v) ps (Message pq) m Unit
+  handleAction = unwrap >>> flip onMatch handleExtraAction
+    { handleInput: \str ->
+        modifyState_ _ { text = str }
 
-    HandleKey ev -> case KE.code ev of
-      "Enter" -> do 
+    , handleKey: \ev -> when (KE.code ev == "Enter") do
         H.liftEffect $ preventDefault (KE.toEvent ev)
         st <- getState
         when (st.text /= "") do
           modifyState_ _ { tags = Set.insert st.text st.tags, text = "" }
-      _ -> 
-        pure unit
-    
-    RemoveTag tag ->
-      modifyState_ \st -> st { tags = Set.delete tag st.tags }
+          handleAction $ handleInput "hello"
 
-    Raise act -> 
-      H.raise (Emit act)
+    , raise: \act -> 
+        H.raise (Emit act)
 
-    Receive { render } -> 
-      modifyStore_ render (\s -> s)
+    , receive: \{ render } -> 
+        modifyStore_ render (\s -> s)
+    }
 
   handleQuery
     :: forall a
      . Query ps a
-    -> H.HalogenM (StateStore pq ps m) (Action pq ps m) ps (Message pq) m (Maybe a)
+    -> H.HalogenM (StateStore pq ps m v) (Action pq ps m v) ps (Message pq) m (Maybe a)
   handleQuery = case _ of
     Send box -> 
       H.HalogenM $ liftF $ H.ChildQuery box
